70f55db9bde140c164120d50e70dbaf95d7bf805
Morten Gerdes <mgd@lunar.app>
2026-02-20 09:35:16 +0100

    Make fuzzyfinder start at the top

--- internal/picker.go
+++ internal/picker.go
@@ -9,7 +9,7 @@ import (
 func Pick(query string, candidates []Candidate) (Candidate, bool, error) {
 	idx, err := fuzzyfinder.Find(candidates, func(i int) string {
 		return CandidateLabel(candidates[i])
-	}, fuzzyfinder.WithQuery(query))
+	}, fuzzyfinder.WithQuery(query), fuzzyfinder.WithCursorPosition(fuzzyfinder.CursorPositionTop))
 	if err != nil {
 		if errors.Is(err, fuzzyfinder.ErrAbort) {
 			return Candidate{}, false, nil
